4a330939db5d50c6fb0aafb6463ba8b41b20bf9d
Akbar Putra Wiratama <akbar08062005@gmail.com>
2025-08-31 18:57:39 +0700

    sync update

--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,20 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-// Halaman pengguna
 Route::get('/pengguna/{id}', function ($id) {
     return view('pengguna', data: ['id' => $id]);
 })->name('pengguna');
 
-// Group untuk manage
 Route::prefix('manage')->group(function () {
     Route::get('/edit/{id}', function ($id) {
         return view('manage.edit-profile', ['id' => $id]);
     })->name('edit-profile');
 });
 
-// About (umum)
 Route::get('/about/{id}', function ($id) {
     return view('about', ['id' => $id]);
 })->name('about');
 
-// Contact (spesifik user)
 Route::get('/contact/{id}', function ($id) {
     return view('contact', ['id' => $id]);
 })->name('contact');
