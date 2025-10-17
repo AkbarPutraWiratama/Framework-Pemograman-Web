ae2926860ee57d6f25f3cc1c6eb374b160eef918
Akbar Putra Wiratama <akbar08062005@gmail.com>
2025-10-17 21:37:51 +0700

    sync update

--- resources/views/master-data/product-master/index.blade.php
+++ resources/views/master-data/product-master/index.blade.php
@@ -53,6 +53,7 @@
                                                 class="inline-flex items-center px-3 py-1 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                                 Edit
                                             </button>
+                                        </form>
                                         {{-- Tombol delete --}}
                                         <form action="{{ route('product-delete', $product->id) }}" method="POST" class="inline">
                                             @csrf
